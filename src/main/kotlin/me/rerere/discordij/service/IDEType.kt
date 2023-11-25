b547a9de4b86c03cd72b754ee4eba64c00faefb7
Nikolas Sturm <stormlp@outlook.de>
2023-11-25 10:34:00 +0100

    change all supported ide's to dynamic icons

--- src/main/kotlin/me/rerere/discordij/service/IDEType.kt
+++ src/main/kotlin/me/rerere/discordij/service/IDEType.kt
@@ -7,51 +7,48 @@ enum class IDEType(
     val title: String,
     val icon: String
 ) {
-    IDEA_ULTIMATE("IntelliJ IDEA Ultimate", "https://raw.githubusercontent.com/Arch-Storm/icons/master/png/intellij.png"),
-    IDEA_COMMUNITY("IntelliJ IDEA Community", "https://raw.githubusercontent.com/Arch-Storm/icons/master/png/intellij.png"),
-    WEBSTORM("WebStorm", "https://raw.githubusercontent.com/Arch-Storm/icons/master/png/webstorm.png"),
-    PYCHARM("PyCharm", "https://raw.githubusercontent.com/Arch-Storm/icons/master/png/pycharm.png"),
-    CLION("CLion", "https://raw.githubusercontent.com/Arch-Storm/icons/master/png/clion.png"),
-    GOLAND("GoLand", "goland"),
-    RIDER("Rider", "rider"),
-    PHPSTORM("PhpStorm", "phpstorm"),
-    ANDROID_STUDIO("Android Studio", "android_studio"),
-    RUSTROVER("RustRover", "rustrover"),
+    IDEA_ULTIMATE("IntelliJ IDEA Ultimate", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/ideau.png"),
+    IDEA_COMMUNITY("IntelliJ IDEA Community", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/ideac.png"),
+    WEBSTORM("WebStorm", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/webstorm.png"),
+    PYCHARM_COMMUNITY("PyCharm Community", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/pycharmc.png"),
+    PYCHARM_PROFESSIONAL("PyCharm Professional", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/pycharmp.png"),
+    CLION("CLion", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/clion.png"),
+    GOLAND("GoLand", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/goland.png"),
+    RIDER("Rider", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/rider.png"),
+    PHPSTORM("PhpStorm", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/phpstorm.png"),
+    ANDROID_STUDIO("Android Studio", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/studio.png"),
+    RUSTROVER("RustRover", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/rustrover.png"),
+    WRITERSIDE("WriterSide","https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/writerside.png"),
+    AQUA("Aqua", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/aqua.png"),
+    DATAGRIP("DataGrip", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/datagrip.png"),
+    DATASPELL("DataSpell", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/dataspell.png"),
+    GATEWAY("Gateway", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/gateway.png"),
+    MPS("MPS", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/mps.png"),
+    RUBYMINE("RubyMine", "https://raw.githubusercontent.com/Arch-Storm/icons/master/ide/rubymine.png"),
     JETBRAINS("JetBrains", "jetbrains"), // FALLBACK
 }
 
-/**
- * Get current IDE type
- *
- * IU - IntelliJ IDEA Ultimate
- * IC - IntelliJ IDEA Community
- * IE - IntelliJ IDEA Educational
- * PS - PhpStorm
- * WS - WebStorm
- * PY - PyCharm Professional
- * PC - PyCharm Community
- * PE - PyCharm Educational
- * RM - RubyMine
- * OC - AppCode
- * CL - CLion
- * GO - GoLand
- * DB - DataGrip
- * RD - Rider
- * AI - Android Studio
- */
 val currentIDEType by lazy {
     val info = ApplicationInfoEx.getInstanceEx()
     when(info.build.productCode) {
         "IC" -> IDEType.IDEA_COMMUNITY
         "IU" -> IDEType.IDEA_ULTIMATE
         "WS" -> IDEType.WEBSTORM
-        "PC", "PY" -> IDEType.PYCHARM
+        "PC" -> IDEType.PYCHARM_COMMUNITY
+        "PY" -> IDEType.PYCHARM_PROFESSIONAL
         "CL" -> IDEType.CLION
         "GO" -> IDEType.GOLAND
         "RD" -> IDEType.RIDER
         "PS" -> IDEType.PHPSTORM
         "AI" -> IDEType.ANDROID_STUDIO
         "RR" -> IDEType.RUSTROVER
+        "WRS" -> IDEType.WRITERSIDE
+        "QA" -> IDEType.AQUA
+        "DB" -> IDEType.DATAGRIP
+        "DS" -> IDEType.DATASPELL
+        "GW" -> IDEType.GATEWAY
+        "MPS" -> IDEType.MPS
+        "RM" -> IDEType.RUBYMINE
         else -> IDEType.JETBRAINS.also {
             DiscordIJ.logger.warn("Unknown IDE type: ${info.build.productCode}")
         }
